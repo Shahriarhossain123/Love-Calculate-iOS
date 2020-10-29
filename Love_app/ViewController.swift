71783cb011943cf8b691cf6c6f430600d2d9ab6f
Shahriar Hossain <9763691+Shahriarhossain123@users.noreply.github.com>
2020-10-29 19:44:24 +0600

    Initial Commit

---
+++ Love_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Love_app
+//
+//  Created by Shishir_Mac on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Love_appTests/Love_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  Love_appTests.swift
+//  Love_appTests
+//
+//  Created by Shishir_Mac on 10/29/20.
+//
+
+import XCTest
+@testable import Love_app
+
+class Love_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
