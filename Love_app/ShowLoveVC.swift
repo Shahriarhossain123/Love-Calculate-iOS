c71dbc8084f81790855cda4d551713f63db6f309
Shahriar Hossain <9763691+Shahriarhossain123@users.noreply.github.com>
2020-10-29 22:59:23 +0600

    Develop

---
+++ Love_app/ShowLoveVC.swift
@@ -0,0 +1,29 @@
+//
+//  ShowLoveVC.swift
+//  Love_app
+//
+//  Created by Shishir_Mac on 10/29/20.
+//
+
+import UIKit
+
+class ShowLoveVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
